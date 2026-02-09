512a2a307c74959e0aec92ed50f5deb00a50fec2
Mohit Bidikar <mohitbidikar4@gmail.com>
2026-02-09 17:13:44 +0000

    Update connect.sh

--- robot/connect.sh
+++ robot/connect.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
+# Get modem index detected by ModemManager
 MODEM_INDEX=$(mmcli -L | grep -i quec | awk -F'/' '{print $NF}' | awk '{print $1}')
+
+# Enable modem
+sudo mmcli -m "$MODEM_INDEX" -e
+
+# Connect using APN
 sudo mmcli -m "$MODEM_INDEX" --simple-connect="apn=srsapn"
+
+echo "Modem connected."
